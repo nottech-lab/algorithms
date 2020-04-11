779df5823183c207e1e49f9218632ef2138f329d
Given Kyambile <givenkyambile@gmail.com>
2020-04-11 18:13:08 +0300

    Implements on sorting

---
+++ bigsorting.py
@@ -0,0 +1,32 @@
+#https://www.hackerrank.com/challenges/big-sorting/problem
+
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the bigSorting function below.
+def bigSorting(unsorted):
+    
+        return sorted((unsorted),key=int)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(raw_input())
+
+    unsorted = []
+
+    for _ in xrange(n):
+        unsorted_item = raw_input()
+        unsorted.append(unsorted_item)
+
+    result = bigSorting(unsorted)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
