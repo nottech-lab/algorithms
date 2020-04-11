dba9049b7c1cb54280841e8b723e9f717d9c1a24
Given Kyambile <givenkyambile@gmail.com>
2020-04-11 16:29:13 +0300

    Implement on solve_me_first

---
+++ solve_me_first.py
@@ -0,0 +1,13 @@
+#https://www.hackerrank.com/challenges/solve-me-first/submissions/code/151848774
+
+
+def solveMeFirst(a,b):
+    res = num1 + num2
+    return res
+	# Hint: Type return a+b below
+
+
+num1 = input()
+num2 = input()
+res = solveMeFirst(num1,num2)
+print (res)
