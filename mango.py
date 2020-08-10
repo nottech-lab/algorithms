7a990e8a2b4bcfd87c8e3bb09c484362d7b97222
Phinamihayo <JosephineMihayo@gmail.co>
2020-08-10 22:01:31 +0300

    added a task challenge

---
+++ mango.py
@@ -0,0 +1,30 @@
+Given an array of integers, find the sum of its elements.
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+# 
+def simpleArraySum(ar):
+    #
+    # Write your code here.
+    #
+    sum=0
+    for x in ar:
+        sum +=x
+    return sum    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
